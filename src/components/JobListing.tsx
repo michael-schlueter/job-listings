90847780f2037d52bed3158a6a77d23a36810e63
Michael Schlueter <schluet0r4life@gmail.com>
2021-12-22 20:13:39 +0100

    changed to mobile design

--- src/components/JobListing.tsx
+++ src/components/JobListing.tsx
@@ -31,9 +31,9 @@ export default function JobListing({ job }:JobType) {
     }
 
   return (
-    <div className="flex bg-white shadow-md m-4 p-6 rounded">
+    <div className={`flex flex-col bg-white shadow-md my-16 mx-10 p-6 rounded ${job.featured && "border-l-4 border-teal-500 border-solid" }`}>
       <div>
-        <img src={job.logo} alt={job.company} />
+        <img className="-mt-16 mb-4 w-20 h-20" src={job.logo} alt={job.company} />
       </div>
       <div className="flex flex-col justify-between ml-4">
           <h3 className="font-bold text-teal-500">{job.company}
@@ -41,18 +41,18 @@ export default function JobListing({ job }:JobType) {
                 <span className="bg-teal-500 text-white m-2 py-1 px-2 rounded-full uppercase">New!</span>
             )}
             {job.featured && (
-                <span className="bg-gray-800 text-white m-2 py-1 px-2 rounded-full uppercase">Featured</span>
+                <span className="bg-gray-800 text-white py-1 px-2 rounded-full uppercase">Featured</span>
             )}
           </h3>
-          <h2 className="font-bold text-xl">{job.position}</h2>
+          <h2 className="font-bold text-xl my-2">{job.position}</h2>
           <p className="text-gray-400">
               {job.postedAt} · {job.contract} · {job.location}
           </p>
       </div>
-      <div className="flex items-center ml-auto">
+      <div className="flex flex-wrap items-center mt-4 mx-4 pt-4 border-t border-gray-500 border-solid">
         {jobTags 
             ? jobTags.map((jobTag) => (
-                <span className="text-teal-500 bg-teal-50 font-bold m-2 p-2 rounded">
+                <span className="text-teal-500 bg-teal-50 font-bold mr-4 mb-4 p-2 rounded">
                     {jobTag}
                 </span>
             )) : ''}

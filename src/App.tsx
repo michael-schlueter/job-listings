aba89e476b730579fbde3fb5f721b661f44163b2
Michael Schlueter <schluet0r4life@gmail.com>
2021-12-27 10:58:55 +0100

    added clear tags button and functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "./API/data.json";
 import JobListing from "./components/JobListing";
 
-// "id": 8,
-// "company": "Insure",
-// "logo": "./images/insure.svg",
-// "new": false,
-// "featured": false,
-// "position": "Junior Frontend Developer",
-// "role": "Frontend",
-// "level": "Junior",
-// "postedAt": "2w ago",
-// "contract": "Full Time",
-// "location": "USA Only",
-// "languages": ["JavaScript"],
-// "tools": ["Vue", "Sass"]
+interface JobType {
+  id: number;
+  company: string;
+  logo: string;
+  new: boolean;
+  featured: boolean;
+  position: string;
+  role: string;
+  level: string;
+  postedAt: string;
+  contract: string;
+  location: string;
+  languages: string[];
+  tools: string[];
+}
 
 function App() {
+  const [filters, setFilters] = useState([]);
+
+  const filterFunction = ({ role, level, tools, languages }: JobType) => {
+    if (filters.length === 0) {
+      return true;
+    }
+
+    const tags = [role, level];
+
+    if (tools) {
+      tags.push(...tools);
+    }
+
+    if (languages) {
+      tags.push(...languages);
+    }
+
+    return tags.some((tag) => filters.includes(tag));
+  };
+
+  const handleTagClick = (tag) => {
+    if (filters.includes(tag)) return;
+
+    setFilters([...filters, tag]);
+  };
+
+  const handleFilterClick = (passedFilter) => {
+    setFilters(filters.filter((f) => f !== passedFilter));
+  };
+
+  const clearFilters = () => {
+    setFilters([]);
+  }
+
+  const filteredJobs = data.filter(filterFunction);
+
   return (
     <div className="App bg-teal-50">
       <header className="bg-teal-700 mb-12">
-        <img src="/images/bg-header-desktop.svg" alt="bg-header" />
+        <img className="w-full" src="/images/bg-header-desktop.svg" alt="bg-header" />
       </header>
+      <div className="container m-auto">
+      {filters.length !== 0 && (
+        <div className="flex bg-white shadow-md my-16 mx-10 p-6 rounded">
+          {filters.map((filter) => (
+            <span
+              onClick={() => handleFilterClick(filter)}
+              className="text-teal-500 bg-teal-50 font-bold mr-4 mb-4 p-2 rounded cursor-pointer lg:mb-0"
+            >
+             ✕ {filter}
+            </span>
+          ))}
+          <button onClick={clearFilters} className="font-bold text-gray-700 ml-auto">Clear</button>
+        </div>
+      )}
       {data.length === 0 ? (
         <p>Jobs are fetching...</p>
       ) : (
-        data.map((job) => <JobListing job={job} key={job.id} />)
+        filteredJobs.map((job) => (
+          <JobListing job={job} key={job.id} handleTagClick={handleTagClick} />
+        ))
       )}
+      </div>
     </div>
   );
 }
--- src/components/JobListing.tsx
+++ src/components/JobListing.tsx
@@ -1,61 +1,75 @@
 import React from "react";
 
 interface JobType {
-    job: {
-        id: number;
-        company: string;
-        logo: string;
-        new: boolean;
-        featured: boolean;
-        position: string;
-        role: string;
-        level: string;
-        postedAt: string;
-        contract: string;
-        location: string;
-        languages: string[];
-        tools: string[];
-    }
+  job: {
+    id: number;
+    company: string;
+    logo: string;
+    new: boolean;
+    featured: boolean;
+    position: string;
+    role: string;
+    level: string;
+    postedAt: string;
+    contract: string;
+    location: string;
+    languages: string[];
+    tools: string[];
+  };
+  handleTagClick: any;
 }
 
-export default function JobListing({ job }:JobType) {
+export default function JobListing({ job, handleTagClick }: JobType) {
+  const jobTags = [job.role, job.level];
 
-    const jobTags = [job.role, job.level];
+  if (job.languages) {
+    jobTags.push(...job.languages);
+  }
 
-    if (job.languages) {
-        jobTags.push(...job.languages);
-    }
-
-    if (job.tools) {
-        jobTags.push(...job.tools);
-    }
+  if (job.tools) {
+    jobTags.push(...job.tools);
+  }
 
   return (
-    <div className={`flex flex-col bg-white shadow-md my-16 mx-10 p-6 rounded ${job.featured && "border-l-4 border-teal-500 border-solid" }`}>
+    <div
+      className={`flex flex-col bg-white shadow-md my-16 mx-10 p-6 rounded ${
+        job.featured && "border-l-4 border-teal-500 border-solid"
+      } lg:flex-row lg:my-4`}
+    >
       <div>
-        <img className="-mt-16 mb-4 w-20 h-20" src={job.logo} alt={job.company} />
+        <img
+          className="-mt-16 mb-4 w-20 h-20 lg:my-0 lg:h-24 lg:w-24"
+          src={job.logo}
+          alt={job.company}
+        />
       </div>
       <div className="flex flex-col justify-between ml-4">
-          <h3 className="font-bold text-teal-500">{job.company}
-            {job.new && (
-                <span className="bg-teal-500 text-white m-2 py-1 px-2 rounded-full uppercase">New!</span>
-            )}
-            {job.featured && (
-                <span className="bg-gray-800 text-white py-1 px-2 rounded-full uppercase">Featured</span>
-            )}
-          </h3>
-          <h2 className="font-bold text-xl my-2">{job.position}</h2>
-          <p className="text-gray-400">
-              {job.postedAt} · {job.contract} · {job.location}
-          </p>
+        <h3 className="font-bold text-teal-500">
+          {job.company}
+          {job.new && (
+            <span className="bg-teal-500 text-white text-sm font-semibold m-2 py-1 px-2 rounded-full uppercase">
+              New!
+            </span>
+          )}
+          {job.featured && (
+            <span className="bg-gray-800 text-white text-sm font-semibold py-1 px-2 rounded-full uppercase">
+              Featured
+            </span>
+          )}
+        </h3>
+        <h2 className="font-bold text-xl my-2">{job.position}</h2>
+        <p className="text-gray-400">
+          {job.postedAt} · {job.contract} · {job.location}
+        </p>
       </div>
-      <div className="flex flex-wrap items-center mt-4 mx-4 pt-4 border-t border-gray-500 border-solid">
-        {jobTags 
-            ? jobTags.map((jobTag) => (
-                <span className="text-teal-500 bg-teal-50 font-bold mr-4 mb-4 p-2 rounded">
-                    {jobTag}
-                </span>
-            )) : ''}
+      <div className="flex flex-wrap items-center mt-4 mx-4 pt-4 border-t border-gray-500 border-solid lg:ml-auto lg:border-0 lg:mt-0 lg:pt-0">
+        {jobTags
+          ? jobTags.map((jobTag) => (
+              <span className="text-teal-500 bg-teal-50 font-bold mr-4 mb-4 p-2 rounded cursor-pointer lg:mb-0" onClick={() => handleTagClick(jobTag)}>
+                {jobTag}
+              </span>
+            ))
+          : ""}
       </div>
     </div>
   );

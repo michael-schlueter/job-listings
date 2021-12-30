95c02fc190c666413304bd5f92403855a738793f
Michael Schlueter <schluet0r4life@gmail.com>
2021-12-30 18:16:19 +0100

    changed colors, added close svg

--- src/App.tsx
+++ src/App.tsx
@@ -56,22 +56,27 @@ function App() {
   const filteredJobs = data.filter(filterFunction);
 
   return (
-    <div className="App bg-teal-50">
-      <header className="bg-teal-700 mb-12">
+    <div className="App">
+      <header className="bg-teal-700 bg-opacity-70 mb-12">
         <img className="w-full" src="/images/bg-header-desktop.svg" alt="bg-header" />
       </header>
       <div className="container m-auto">
       {filters.length !== 0 && (
         <div className="flex bg-white shadow-md -my-24 mb-16 mx-10 p-6 rounded relative z-10">
-          {filters.map((filter) => (
-            <span
-              onClick={() => handleFilterClick(filter)}
-              className="text-teal-500 bg-teal-50 font-bold mr-4 mb-4 p-2 rounded cursor-pointer lg:mb-0"
-            >
-             ✕ {filter}
-            </span>
+          {filters.map((filter, index) => (
+            // <span
+            //   onClick={() => handleFilterClick(filter)}
+            //   className="text-desaturated-dark-cyan bg-teal-50 font-bold mr-4 mb-4 p-2 rounded cursor-pointer lg:mb-0"
+            // >
+            //  ✕ {filter}
+            // </span>
+            <h3 key={index} className="flex justify-center items-center border-0 rounded-md pt-0 pr-0 pb-0 pl-2 mt-4 mr-4 text-desaturated-dark-cyan bg-teal-50 font-bold ">{filter}
+                                <button onClick={() => {handleFilterClick(filter)}} className="pointer flex justify-center items-center border-0 w-8 h-8 ml-2 bg-desaturated-dark-cyan rounded-tr-sm rounded-br-sm hover:bg-very-dark-grayish-cyan">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14"><title>remove filter</title><path fill="#FFF" fillRule="evenodd" d="M11.314 0l2.121 2.121-4.596 4.596 4.596 4.597-2.121 2.121-4.597-4.596-4.596 4.596L0 11.314l4.596-4.597L0 2.121 2.121 0l4.596 4.596L11.314 0z"/></svg>
+                                </button>
+                            </h3> 
           ))}
-          <button onClick={clearFilters} className="font-bold text-gray-700 ml-auto">Clear</button>
+          <button onClick={clearFilters} className="font-bold text-gray-400 ml-auto mr-10 hover:text-desaturated-dark-cyan hover:underline">Clear</button>
         </div>
       )}
       {data.length === 0 ? (
--- src/components/JobListing.tsx
+++ src/components/JobListing.tsx
@@ -33,7 +33,7 @@ export default function JobListing({ job, handleTagClick }: JobType) {
   return (
     <div
       className={`flex flex-col bg-white shadow-md my-16 mx-10 p-6 rounded ${
-        job.featured && "border-l-4 border-teal-500 border-solid"
+        job.featured && "border-l-4 border-desaturated-dark-cyan border-solid"
       } lg:flex-row lg:my-4`}
     >
       <div>
@@ -44,20 +44,20 @@ export default function JobListing({ job, handleTagClick }: JobType) {
         />
       </div>
       <div className="flex flex-col justify-between ml-4">
-        <h3 className="font-bold text-teal-500">
+        <h3 className="font-bold text-desaturated-dark-cyan">
           {job.company}
           {job.new && (
-            <span className="bg-teal-500 text-white text-sm font-semibold m-2 py-1 px-2 rounded-full uppercase">
+            <span className="bg-desaturated-dark-cyan text-white text-sm font-semibold m-2 py-1 px-2 rounded-full uppercase">
               New!
             </span>
           )}
           {job.featured && (
-            <span className="bg-gray-800 text-white text-sm font-semibold py-1 px-2 rounded-full uppercase">
+            <span className="bg-very-dark-grayish-cyan text-white text-sm font-semibold py-1 px-2 rounded-full uppercase">
               Featured
             </span>
           )}
         </h3>
-        <h2 className="font-bold text-xl my-2">{job.position}</h2>
+        <h2 className="font-bold text-xl text-very-dark-grayish-cyan my-2 hover:text-desaturated-dark-cyan cursor-pointer">{job.position}</h2>
         <p className="text-gray-400">
           {job.postedAt} · {job.contract} · {job.location}
         </p>
@@ -65,7 +65,7 @@ export default function JobListing({ job, handleTagClick }: JobType) {
       <div className="flex flex-wrap items-center mt-4 mx-4 pt-4 border-t border-gray-500 border-solid lg:ml-auto lg:border-0 lg:mt-0 lg:pt-0">
         {jobTags
           ? jobTags.map((jobTag) => (
-              <span className="text-teal-500 bg-teal-50 font-bold mr-4 mb-4 p-2 rounded cursor-pointer lg:mb-0" onClick={() => handleTagClick(jobTag)}>
+              <span className="text-desaturated-dark-cyan bg-light-grayish-cyan font-bold mr-4 mb-4 p-2 rounded cursor-pointer lg:mb-0 hover:bg-desaturated-dark-cyan hover:text-white" onClick={() => handleTagClick(jobTag)}>
                 {jobTag}
               </span>
             ))

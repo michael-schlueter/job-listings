f16da3d5b6ddd4b01e70fadd3c6db08cbc082a9f
Michael Schlueter <schluet0r4life@gmail.com>
2021-12-31 11:58:32 +0100

    responsive spacing for tags and clear button

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ function App() {
       tags.push(...languages);
     }
 
-    return filters.every(filter => tags.includes(filter));
+    return filters.every((filter) => tags.includes(filter));
   };
 
   const handleTagClick = (tag: string) => {
@@ -51,41 +51,70 @@ function App() {
 
   const clearFilters = () => {
     setFilters([]);
-  }
+  };
 
   const filteredJobs = data.filter(filterFunction);
 
   return (
     <div className="App">
       <header className="bg-teal-700 bg-opacity-70 mb-12">
-        <img className="w-full" src="/images/bg-header-desktop.svg" alt="bg-header" />
+        <img
+          className="w-full"
+          src="/images/bg-header-desktop.svg"
+          alt="bg-header"
+        />
       </header>
       <div className="container m-auto">
-      {filters.length !== 0 && (
-        <div className="flex bg-white shadow-md -my-[88px] mb-16 mx-10 p-6 rounded relative z-10">
-          {filters.map((filter, index) => (
-            // <span
-            //   onClick={() => handleFilterClick(filter)}
-            //   className="text-desaturated-dark-cyan bg-teal-50 font-bold mr-4 mb-4 p-2 rounded cursor-pointer lg:mb-0"
-            // >
-            //  ✕ {filter}
-            // </span>
-            <h3 key={index} className="flex justify-center items-center border-0 rounded-md pt-0 pr-0 pb-0 pl-2 mr-4 text-desaturated-dark-cyan bg-teal-50 font-bold ">{filter}
-                                <button onClick={() => {handleFilterClick(filter)}} className="pointer flex justify-center items-center border-0 w-8 h-8 ml-2 bg-desaturated-dark-cyan rounded-tr-sm rounded-br-sm hover:bg-very-dark-grayish-cyan">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14"><title>remove filter</title><path fill="#FFF" fillRule="evenodd" d="M11.314 0l2.121 2.121-4.596 4.596 4.596 4.597-2.121 2.121-4.597-4.596-4.596 4.596L0 11.314l4.596-4.597L0 2.121 2.121 0l4.596 4.596L11.314 0z"/></svg>
-                                </button>
-                            </h3> 
-          ))}
-          <button onClick={clearFilters} className="font-bold text-gray-400 ml-auto mr-10 hover:text-desaturated-dark-cyan hover:underline">Clear</button>
-        </div>
-      )}
-      {data.length === 0 ? (
-        <p>Jobs are fetching...</p>
-      ) : (
-        filteredJobs.map((job) => (
-          <JobListing job={job} key={job.id} handleTagClick={handleTagClick} />
-        ))
-      )}
+        {filters.length !== 0 && (
+          <div className="flex bg-white shadow-md -my-[92px] mb-16 mx-10 p-6 rounded relative z-10">
+            <div className="flex flex-wrap">
+              {filters.map((filter, index) => (
+                <h3
+                  key={index}
+                  className="flex justify-center items-center border-0 rounded-md pt-0 pr-0 pb-0 pl-2 mr-4 my-1 text-desaturated-dark-cyan bg-light-grayish-cyan-filter-tablets font-bold "
+                >
+                  {filter}
+                  <button
+                    onClick={() => {
+                      handleFilterClick(filter);
+                    }}
+                    className="pointer flex justify-center items-center border-0 w-8 h-8 ml-2 bg-desaturated-dark-cyan rounded-tr-sm rounded-br-sm hover:bg-very-dark-grayish-cyan"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="14"
+                      height="14"
+                    >
+                      <title>remove filter</title>
+                      <path
+                        fill="#FFF"
+                        fillRule="evenodd"
+                        d="M11.314 0l2.121 2.121-4.596 4.596 4.596 4.597-2.121 2.121-4.597-4.596-4.596 4.596L0 11.314l4.596-4.597L0 2.121 2.121 0l4.596 4.596L11.314 0z"
+                      />
+                    </svg>
+                  </button>
+                </h3>
+              ))}
+            </div>
+            <button
+              onClick={clearFilters}
+              className="font-bold text-gray-400 ml-auto mr-10 hover:text-desaturated-dark-cyan hover:underline"
+            >
+              Clear
+            </button>
+          </div>
+        )}
+        {data.length === 0 ? (
+          <p>Jobs are fetching...</p>
+        ) : (
+          filteredJobs.map((job) => (
+            <JobListing
+              job={job}
+              key={job.id}
+              handleTagClick={handleTagClick}
+            />
+          ))
+        )}
       </div>
     </div>
   );
